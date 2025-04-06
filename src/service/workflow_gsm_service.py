44cea8aa00491293884fc189e057296a6403c32f
pgorade <pgorade@slb.com>
2025-04-06 20:25:06 +0530

    code refactor

--- src/service/workflow_gsm_service.py
+++ src/service/workflow_gsm_service.py
@@ -43,8 +43,15 @@ async def on_request_start(
 TIME_OUT = 60
 
 
+@retry(exceptions=Exception, tries=2, delay=1, logger=logger)
 def get_token(env: str):
-    scope = f"{keyvault[env]["scope"]} {keyvault[env]["client_id"]}" if env.startswith('ltops') else keyvault[env]["scope"]
+    if keyvault.get(env).get("bearer-token") not in [None, ""]:
+        return keyvault[env]["bearer-token"]
+    scope_osdu = keyvault[env]["scope"]
+    scope_client_id = keyvault[env]["client_id"]
+    scope_ccm = keyvault["scope_ccm"]
+    scope = f"{scope_osdu} {scope_client_id} {scope_ccm}" if env.endswith('ltops') else keyvault[env][
+        "scope"]
     response = requests.request(method="POST",
                                 url=keyvault[env]["token_url"],
                                 headers={"content-type": "application/x-www-form-urlencoded"},
@@ -52,11 +59,11 @@ def get_token(env: str):
 
     if response.status_code == 200:
         print(f"********* Token Generated Successfully ************")
-        response_dict = json.loads(response.text)
-        return "Bearer " + response_dict["access_token"]
+        response_json = response.json()
+        return "Bearer " + response_json["access_token"]
     else:
-        print(f"Error occurred while creating token. {response.text}")
-        # exit(1)
+        msg = f"Error occurred while fetching token from {keyvault[env]["token_url"]=}. {response.status_code=} {response.text}"
+        print(f"{msg=}")
 
 
 def create_workflow_payload(env, dag):
